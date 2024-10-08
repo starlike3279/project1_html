33f935cc630ae346febbbdc77c00490506e8ab10
taeho5825 <xogh3322@naver.comm>
2024-10-08 10:39:41 +0900

    html1, html2, html3

---
+++ src/main/java/com/example/hlwhtmlproject1/Hlwhtmlproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.hlwhtmlproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hlwhtmlproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hlwhtmlproject1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/hlwhtmlproject1/MainController.java
@@ -0,0 +1,23 @@
+package com.example.hlwhtmlproject1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/signup")
+    public String index(){
+        return "signup";
+    }
+
+    @GetMapping("/login")
+    public String index2(){
+        return "login";
+    }
+
+    @GetMapping("/todolist_inquiry")
+    public String index3(){
+        return "todolist_inquiry";
+    }
+}
